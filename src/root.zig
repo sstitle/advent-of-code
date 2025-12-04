2629d8f2f0ede2ca07175818b6885befd12f2209
Sam Title <samstitle@gmail.com>
2025-12-04 00:33:05 -0500

    chore: keep-sorted

--- src/root.zig
+++ src/root.zig
@@ -1,5 +1,8 @@
+// keep-sorted start
 pub const day01 = @import("day01.zig");
+pub const day02 = @import("day02.zig");
 pub const utils = @import("utils.zig");
+// keep-sorted end
 
 test {
     @import("std").testing.refAllDecls(@This());
