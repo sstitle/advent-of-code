aaffeea6df202757ee5c86f6848c1ece6dad6b10
Sam Title <samstitle@gmail.com>
2025-12-07 00:10:27 -0500

    feat: first solution for day 3

--- src/day03.zig
+++ src/day03.zig
@@ -10,11 +10,36 @@ pub const day = Day([]const u8, u64){
     },
 };
 
-pub fn solve(joltages: []const []const u8) !u64 {
-    for (joltages) |joltage| {
-        std.debug.print("Processing joltage: {s}\n", .{joltage});
+const solveError = error{
+    tooFewValues,
+};
+
+pub fn findLargestPair(joltage: []const u8) !u64 {
+    if (joltage.len < 2) {
+        return solveError.tooFewValues;
     }
-    return 0;
+    var max_value: u64 = 0;
+    for (0..joltage.len) |i| {
+        for (i + 1..joltage.len) |j| {
+            const a: u64 = joltage[i] - '0';
+            const b: u64 = joltage[j] - '0';
+            const value = a * 10 + b;
+            if (value > max_value) {
+                max_value = value;
+            }
+        }
+    }
+    return max_value;
+}
+
+pub fn solve(joltages: []const []const u8) !u64 {
+    var total: u64 = 0;
+    for (joltages) |joltage| {
+        const value = try findLargestPair(joltage);
+        std.debug.print("{s} -> {d}\n", .{ joltage, value });
+        total += value;
+    }
+    return total;
 }
 
 pub fn getExampleJoltages() []const []const u8 {
